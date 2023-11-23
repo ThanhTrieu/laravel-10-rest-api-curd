102277ec6c011e1592b88a3af9fa0e5f772ec0a5
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2023-11-23 23:27:17 +0700

    first commit

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Posts::all();
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        $posts = Posts::create($request->all());
+        return new PostResource($posts);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePostRequest $request, Posts $posts)
+    {
+        $posts->update($request->all());
+        return new PostResource($posts);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Posts $posts)
+    {
+        $posts->delete();
+        return response(null,204);
+    }
+}
